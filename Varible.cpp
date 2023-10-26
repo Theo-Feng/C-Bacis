dad8b999e0487671987ed027afd372a224d5fb23
Theo-Feng <147600433+Theo-Feng@users.noreply.github.com>
2023-10-26 01:43:09 -0700

    Add files via upload

---
+++ Varible.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string Name = "Theo";
+    int Age = 18;
+
+    cout << "My name is " << Name << endl;
+    cout << "I am " << Age << " years old" << endl;
+    cout << "I like my name, " << Name << endl;
+    cout << "I like being " << Age << " years old" << endl;
+
+    return 0;
+}
