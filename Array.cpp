3b3a8920c5bb13e92b4a144c7f417bed5828d163
Theo-Feng <147600433+Theo-Feng@users.noreply.github.com>
2023-11-29 00:07:52 -0800

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+    int luckynumber[] = {1,2,3,5,8,13};
+    
+    luckynumber [0] = 88;
+    
+    cout << luckynumber [0];
+    
+    return 0;
+}
