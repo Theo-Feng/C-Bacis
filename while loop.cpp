d441fb4264fb88fdec63e2a85d787ca702619d57
Theo-Feng <147600433+Theo-Feng@users.noreply.github.com>
2023-11-29 21:23:20 -0800

    Add files via upload

---
+++ while loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int index = 1;
+    while (index <= 5){ //Also do while loop, which is run first and then check condition
+        cout << index <<endl;
+        index ++;
+    }
+    return 0;
+}
